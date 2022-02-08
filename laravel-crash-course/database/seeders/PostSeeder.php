41648c7c79953458a4a8a5b470affbf0b1d0c7d2
Vladislav Alexeev <vladislaw.al2015@yandex.ru>
2022-02-08 17:13:48 +0300

    Создание представлений (views) и работа с БД (создание таблиц, наполнение их тестовыми данными) - обучение по видеоролику

---
+++ laravel-crash-course/database/seeders/PostSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 20; $i++){
+            DB::table('posts')->insert([
+                'category_id' => rand(1, 10),
+                'title' => 'Пост '.$i,
+                'description' => 'Описание поста № '.$i,
+                'text' => '<p>Текст поста, текст поста, текст поста... <b>(пост № '.$i.')</b></p>',
+                'slug' => 'post-'.$i,
+            ]);
+        }
+    }
+}
